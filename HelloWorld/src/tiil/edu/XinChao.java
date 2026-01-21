76310740766e7c995bf87c888e1c595617e2d9ba
ntnhan2111 <nhan.nth.63cntt@ntu.edu.vn>
2026-01-21 15:17:13 +0700

    Cập nhật code
    Sửa mới và thêm dòng

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -14,7 +14,7 @@ public class XinChao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, vầ đang thử nghiệm quản lý mã nguồn với GIT");
-
+		System.out.print("Tôi thêm một dòng mới , và sửa lại lệnh Java ở trên");
 	}
 
 }
