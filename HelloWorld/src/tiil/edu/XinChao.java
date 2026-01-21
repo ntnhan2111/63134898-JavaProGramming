4dee6ed93ff7226311781219829db3429ba501f9
ntnhan2111 <nhan.nth.63cntt@ntu.edu.vn>
2026-01-21 15:13:17 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, vầ đang thử nghiệm quản lý mã nguồn với GIT");
+
+	}
+
+}
